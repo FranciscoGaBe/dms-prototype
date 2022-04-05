1892ece7219c0a77eb93074ed5947d5016e360bf
Francisco Garrido <francisco.garrido.bear@gmail.com>
2022-04-05 19:12:56 +0200

    Feat: LeftBarMenu now shows a tooltip
    When scrolling over the options, it will show 'not implemented' to indicate that it's not implemented

--- src/components/DMSLeftBarMenu.vue
+++ src/components/DMSLeftBarMenu.vue
@@ -5,16 +5,20 @@
 			class="w-44 bg-white bottom-0 absolute rounded border-2 border-gray-200 shadow"
 			style="left: 120%"
 		>
-			<div
-				v-for="button in buttons"
+      <DMSTooltip
+        v-for="button in buttons"
 				:key="button.name"
 				class="flex items-center w-full px-2 py-1 cursor-not-allowed hover:bg-gray-100"
-			>
-				<span class="flex-shrink-0 mr-2 w-5 text-center">
-					<font-awesome-icon :icon="button.icon" />
-				</span>
-				<span class="flex-shrink-0">{{ button.name }}</span>
-			</div>
+        position="right"
+      >
+        <template #activator>
+          <span class="flex-shrink-0 mr-2 w-5 text-center">
+            <font-awesome-icon :icon="button.icon" />
+          </span>
+          <span class="flex-shrink-0">{{ button.name }}</span>
+        </template>
+        <span>Not implemented</span>
+      </DMSTooltip>
 			<div class="border border-gray-200"></div>
 			<div
 				class="flex items-center w-full px-2 py-2 cursor-pointer hover:bg-red-500 hover:text-white"
@@ -30,25 +34,27 @@
 </template>
 
 <script>
+import DMSTooltip from "./DMSTooltip.vue";
 	export default {
-		name: "DMSLeftBarMenu",
-		props: {
-			show: {
-				type: Boolean,
-				default: false,
-			},
-		},
-		data: () => ({
-			buttons: [
-				{ icon: "cog", name: "Control Panel" },
-				{ icon: "user", name: "Entities" },
-				{ icon: "eye", name: "OCR" },
-			],
-		}),
-		methods: {
-			logout: function () {
-				this.$router.replace("/");
-			},
-		},
-	};
+    name: "DMSLeftBarMenu",
+    props: {
+        show: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    data: () => ({
+        buttons: [
+            { icon: "cog", name: "Control Panel" },
+            { icon: "user", name: "Entities" },
+            { icon: "eye", name: "OCR" },
+        ],
+    }),
+    methods: {
+        logout: function () {
+            this.$router.replace("/");
+        },
+    },
+    components: { DMSTooltip }
+};
 </script>
